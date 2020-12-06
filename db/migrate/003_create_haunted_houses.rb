4608f0e0c40073c08e9c1568daf766d268831a80
Christian Simon <csimon1127@gmail.com>
2020-12-06 23:17:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,12 +3,12 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   
   def change
-    create_table :HauntedHouses do |t|
+    create_table :haunted_houses do |t|
       t.string :name 
       t.string :location 
       t.string :theme 
       t.float :price
-      t.string :family_friendly
+      t.boolean :family_friendly
       t.datetime :opening_date 
       t.datetime :closing_date
       t.string :description
